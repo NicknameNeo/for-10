9700483e14fa04e4c512e850079adf6d922b3649
NicknameNeo <abdulloevaslan96@gmail.com>
2024-12-06 19:06:22 +0500

    первый коммит

---
+++ for 9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace for_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double sum = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                sum += 1.0 / i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
